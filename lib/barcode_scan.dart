df91299f71dd7a5779fca98d10e36cf9dea81169
Ethras <vladimirmbassi@gmail.com>
2018-05-09 09:31:17 +0200

    added basic ScanOptions

--- lib/barcode_scan.dart
+++ lib/barcode_scan.dart
@@ -6,5 +6,19 @@ class BarcodeScanner {
   static const CameraAccessDenied = 'PERMISSION_NOT_GRANTED';
   static const MethodChannel _channel =
       const MethodChannel('com.apptreesoftware.barcode_scan');
-  static Future<String> scan() async => await _channel.invokeMethod('scan');
+  static Future<String> scan({ScanOptions options}) async {
+    if (options == null) {
+      options = ScanOptions();
+    }
+    return await _channel.invokeMethod('scan', options.toMap());
+  }
+}
+
+class ScanOptions {
+  bool waitTap = false;
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> arguments = {'waitTap': waitTap};
+    return arguments;
+  }
 }
